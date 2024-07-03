6dca949da82d53f6e0eff5889ae32b500daf2322
ganeshashivarama <32331342+ganeshashivarama@users.noreply.github.com>
2024-07-03 19:03:28 +0530

    Update recipes.py - mutton
    Added new mutton 2 recipes

--- recipes.py
+++ recipes.py
@@ -1,19 +1,21 @@
 #Welcome to my Recipes book
 
 veg_recipes = {
-  1: 'mixveg red pasta'
-  2: 'mixveg white pasta'
-  3: 'veg maggie'
-  4. 'veg cheese maggie'
-  5: 'paneer pasta'
+  1: 'mixveg red pasta',
+  2: 'mixveg white pasta',
+  3: 'veg maggie',
+  4. 'veg cheese maggie',
+  5: 'paneer pasta',
   6: 'paneer cheese pasta'
 }
 
 non_veg_recipes = {
-  1: 'egg pasta'
-  2: 'egg cheese pasta'
-  3: 'egg maggie'
-  4: 'egg cheese maggie'
-  5: 'chicken pasta'
-  6: 'chicken cheese pasta'
+  1: 'egg pasta',
+  2: 'egg cheese pasta',
+  3: 'egg maggie',
+  4: 'egg cheese maggie',
+  5: 'chicken pasta',
+  6: 'chicken cheese pasta',
+  7: 'mutton chilly',
+  8: 'mutton kabab'
 }
